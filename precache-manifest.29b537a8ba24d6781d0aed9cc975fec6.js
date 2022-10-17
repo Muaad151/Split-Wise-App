611058d6dc212c7f350e51bff8c257b1131d75be
Muaad Akmal <86738725+Muaad151@users.noreply.github.com>
2022-10-17 15:20:56 +0530

    Add files via upload

---
+++ precache-manifest.29b537a8ba24d6781d0aed9cc975fec6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0e7d45dbf9f9d423330b",
+    "url": "/static/css/main.54c5bab6.chunk.css"
+  },
+  {
+    "revision": "0e7d45dbf9f9d423330b",
+    "url": "/static/js/main.0e7d45db.chunk.js"
+  },
+  {
+    "revision": "84cf99745ce0980187aa",
+    "url": "/static/js/1.84cf9974.chunk.js"
+  },
+  {
+    "revision": "ab4bc3e9493cea8659c6",
+    "url": "/static/js/2.ab4bc3e9.chunk.js"
+  },
+  {
+    "revision": "0b9ef2e8599e90f9bb62",
+    "url": "/static/js/runtime~main.0b9ef2e8.js"
+  },
+  {
+    "revision": "8e901761c1fe81a97db70c9a9e76fec7",
+    "url": "/index.html"
+  }
+];
